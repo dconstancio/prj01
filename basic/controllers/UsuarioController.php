049edbac6c24d8a97ab06f30e30380b26d58852e
dconstancio <diego.constancio@gmail.com>
2015-03-23 21:20:26 -0300

    M

---
+++ basic/controllers/UsuarioController.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace app\controllers;
+
+class UsuarioController extends \yii\web\Controller
+{
+}
---
+++ basic/models/Perfil.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "perfil".
+ *
+ * @property integer $id
+ * @property string $descricao
+ *
+ * @property Usuario[] $usuarios
+ */
+class Perfil extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'perfil';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['descricao'], 'string', 'max' => 45]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'descricao' => 'Descricao',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUsuarios()
+    {
+        return $this->hasMany(Usuario::className(), ['cd_perfil' => 'id']);
+    }
+}
